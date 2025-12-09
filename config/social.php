54271235408ba34f9cd4386ca9bc7f04a1c23c68
T Rock <terence.chia@gmail.com>
2025-12-09 16:17:40 +1100

    config options rename

--- config/social.php
+++ config/social.php
@@ -3,10 +3,10 @@
 return [
     'providers' => [
         'google' => [
-            'enabled' => (bool) env('OAUTH_GOOGLE_ENABLED', true),
+            'enabled' => (bool) env('GOOGLE_OAUTH_ENABLED', true),
         ],
         'meta' => [
-            'enabled' => (bool) env('OAUTH_META_ENABLED', false),
+            'enabled' => (bool) env('META_OAUTH_ENABLED', false),
         ],
     ],
 ];
